9e67af2dca2934a865c8ed05c650d8260af5192f
Grzegorz Marcinek <jan.dziuba@gmail.com>
2020-01-13 22:05:21 +0100

    remove unconnected social media icons

--- src/features/footer/footer.component.tsx
+++ src/features/footer/footer.component.tsx
@@ -5,14 +5,12 @@ export class Footer extends React.Component<{}, {}> {
   render() {
     return (
       <div id='footer' className='footer'>
-        <i className='demo-icon icon-vimeo'></i>
         <a href='https://www.instagram.com/gmarcinek/' target='blank'>
           <i className='demo-icon icon-instagram'></i>
         </a>
         <a href='https://www.facebook.com/aeropan/' target='blank'>
           <i className='demo-icon icon-facebook-rect'></i>
         </a>
-        <i className='demo-icon icon-twitter-bird'></i>
       </div>
     )
   }
