1831e9aeaaf8cb2422af103b2ea55a5b5d876458
Grzegorz Marcinek <jan.dziuba@gmail.com>
2020-01-20 00:33:23 +0100

    fix position of film page header

--- src/app.js
+++ src/app.js
@@ -7,8 +7,8 @@ function app() {
   const width = window.innerWidth
   const height = window.innerHeight
   const videoFile = width - height < 0
-    ? 'http://serwer2083856.home.pl/NARODOWY_SH030_INSTA_v001.mp4'
-    : 'http://serwer2083856.home.pl/NARODOWY_MAIN_HD_v001.mp4'
+    ? 'NARODOWY_SH030_INSTA_v001.mp4'
+    : 'NARODOWY_MAIN_HD_v001.mp4'
   const imageSource = width - height < 0
     ? MOBILE_IMAGES
     : DESCTOP_IMAGES
@@ -84,7 +84,7 @@ function app() {
 
                   <div className='fp-content t-white' style={{
                     position: 'absolute',
-                    bottom: '2%',
+                    top: '2%',
                     left: '50%',
                     width: '100%',
                     transform: 'translate(-50%)'
