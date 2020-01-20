4a5ed3ddfbb3519c4aa7348682c13e88b4198fe2
Grzegorz Marcinek <jan.dziuba@gmail.com>
2020-01-20 02:21:44 +0100

    move logo to top on main page, wirkaround paddings for smaller devices

--- src/app.js
+++ src/app.js
@@ -102,8 +102,9 @@ function app() {
                       <h1 style={{ textAlign: 'center', marginBottom: '0', width: '100%' }}>AEROPAN <span style={{ fontWeight: '200' }}>PHOTOGRAPHY</span></h1>
                       <div style={{ textAlign: 'center' }}>PRESENTS</div>
 
+                      <div className='break-8' />
+
                       <div className='article-container'>
-                        <div className='break-8' />
                         <div className='article'>
                           <h2>Exterior photography</h2>
                           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
