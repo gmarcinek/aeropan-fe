a8589001d9e49c9ce36a0972aac27be44cfbc99b
Grzegorz Marcinek <jan.dziuba@gmail.com>
2020-02-02 22:09:42 +0100

    add uniqe iframe ids

--- src/features/google.maps.page/google.maps.page.component.jsx
+++ src/features/google.maps.page/google.maps.page.component.jsx
@@ -15,7 +15,7 @@ export default class GoogleMapsPageComponent extends React.Component {
 
           <div className='slide'>
             <div className='fp-bg'>
-              <iframe className='covered'
+              <iframe title='iframe-1' className='covered'
                 src="https://www.google.com/maps/embed?pb=!4v1580171896473!6m8!1m7!1sCAoSLEFGMVFpcE55OGFlTlQ3ZzBlOUlSaEJYRURPTW82T3U2WkpDekZDR0RBNTJ6!2m2!1d-13.7503812!2d-71.21146379999999!3f0!4f0!5f0.7820865974627469"
                 width='100%' height='100%' frameBorder='0' style={{ border: 0 }}></iframe>
             </div>
@@ -26,7 +26,7 @@ export default class GoogleMapsPageComponent extends React.Component {
 
           <div className='slide'>
             <div className='fp-bg'>
-              <iframe className='covered' src="https://www.google.com/maps/embed?pb=!4v1580172070946!6m8!1m7!1sCAoSLEFGMVFpcE04RVR5LURXd2RZRWp2WkdyLWJyaS1JNFJOVXducW1QS2Q3dGo5!2m2!1d-13.5048046!2d-71.8179153!3f339.42459808865635!4f-18.720280354658925!5f0.4000000000000002"
+              <iframe title='iframe-2' className='covered' src="https://www.google.com/maps/embed?pb=!4v1580172070946!6m8!1m7!1sCAoSLEFGMVFpcE04RVR5LURXd2RZRWp2WkdyLWJyaS1JNFJOVXducW1QS2Q3dGo5!2m2!1d-13.5048046!2d-71.8179153!3f339.42459808865635!4f-18.720280354658925!5f0.4000000000000002"
                 width='100%' height='100%' frameBorder='0' style={{ border: 0 }}></iframe>
             </div>
             <a href={'/#googlemaps'} className='fp-content ap-galery__header-top t-white'>
@@ -36,7 +36,7 @@ export default class GoogleMapsPageComponent extends React.Component {
 
           <div className='slide'>
             <div className='fp-bg'>
-              <iframe className='covered' src="https://www.google.com/maps/embed?pb=!4v1580672788580!6m8!1m7!1sCAoSLEFGMVFpcE1RalpXcXlQVHg3dl9iY1JVOWxCSzRpYk9zWGZsTURNdm5OUEwz!2m2!1d-13.7364578!2d-71.235317!3f10.551538597366996!4f-3.7984544113729726!5f0.7820865974627469"
+              <iframe title='iframe-3' className='covered' src="https://www.google.com/maps/embed?pb=!4v1580672788580!6m8!1m7!1sCAoSLEFGMVFpcE1RalpXcXlQVHg3dl9iY1JVOWxCSzRpYk9zWGZsTURNdm5OUEwz!2m2!1d-13.7364578!2d-71.235317!3f10.551538597366996!4f-3.7984544113729726!5f0.7820865974627469"
                 width='100%' height='100%' frameBorder='0' style={{ border: 0 }}></iframe>
             </div>
             <a href={'/#googlemaps'} className='fp-content ap-galery__header-top t-white'>
@@ -46,7 +46,7 @@ export default class GoogleMapsPageComponent extends React.Component {
 
           <div className='slide'>
             <div className='fp-bg'>
-              <iframe className='covered' src="https://www.google.com/maps/embed?pb=!4v1580672967153!6m8!1m7!1sCAoSLEFGMVFpcE5qYTNhc1A1djhod3dWZFZ4Y1hGaHBFTWF2Ym1PTll6eXZMdmZW!2m2!1d-13.519444!2d-71.78055599999999!3f306.74885146792536!4f-2.1940771661314358!5f1.5088161619395466"
+              <iframe title='iframe-4' className='covered' src="https://www.google.com/maps/embed?pb=!4v1580672967153!6m8!1m7!1sCAoSLEFGMVFpcE5qYTNhc1A1djhod3dWZFZ4Y1hGaHBFTWF2Ym1PTll6eXZMdmZW!2m2!1d-13.519444!2d-71.78055599999999!3f306.74885146792536!4f-2.1940771661314358!5f1.5088161619395466"
                 width='100%' height='100%' frameBorder='0' style={{ border: 0 }}></iframe>
             </div>
             <a href={'/#googlemaps'} className='fp-content ap-galery__header-top t-white'>
@@ -56,7 +56,7 @@ export default class GoogleMapsPageComponent extends React.Component {
 
           <div className='slide'>
             <div className='fp-bg'>
-              <iframe className='covered' src="https://www.google.com/maps/embed?pb=!4v1580673269970!6m8!1m7!1sCAoSLEFGMVFpcE85RnFXaS1LMXpHSGtFUDFMN0d5bUlnbXZRQVRabXpxR2IyWXpV!2m2!1d-13.435385!2d-71.8117985!3f2.979067256678189!4f-21.17089939640293!5f0.7820865974627469"
+              <iframe title='iframe-5' className='covered' src="https://www.google.com/maps/embed?pb=!4v1580673269970!6m8!1m7!1sCAoSLEFGMVFpcE85RnFXaS1LMXpHSGtFUDFMN0d5bUlnbXZRQVRabXpxR2IyWXpV!2m2!1d-13.435385!2d-71.8117985!3f2.979067256678189!4f-21.17089939640293!5f0.7820865974627469"
                 width='100%' height='100%' frameBorder='0' style={{ border: 0 }}></iframe>
             </div>
             <a href={'/#googlemaps'} className='fp-content ap-galery__header-top t-white'>
