b20f3607efdcfdb73b3d34b06ee0369dee0ea698
Grzegorz Marcinek <jan.dziuba@gmail.com>
2021-03-20 12:51:25 +0100

    add new images

--- src/features/film.page/film.page.component.jsx
+++ src/features/film.page/film.page.component.jsx
@@ -3,7 +3,7 @@ import VideoSlide from '../../components/video.slide/video.slide.component'
 
 export default class FilmPageComponent extends React.Component {
   render() {
-    const stadionVideo = 'https://serwer2140934.home.pl/reel.001sd.mp4'
+    const stadionVideo = 'https://serwer2140934.home.pl/AEROPAN_MAIN_001.mp4'
 
     return (
       <div className='section'>
