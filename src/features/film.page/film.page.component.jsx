6befc1afa81b3eadef80896ba28e74a3bca10cd2
Grzegorz Marcinek <jan.dziuba@gmail.com>
2021-08-30 13:20:14 +0200

    change main video

--- src/features/film.page/film.page.component.jsx
+++ src/features/film.page/film.page.component.jsx
@@ -3,11 +3,11 @@ import VideoSlide from '../../components/video.slide/video.slide.component'
 
 export default class FilmPageComponent extends React.Component {
   render() {
-    const stadionVideo = 'https://serwer2140934.home.pl/AEROPAN_MAIN_001.mp4'
+    const stadionVideo = 'https://serwer2140934.home.pl/NARODOWY_SH030_HD_v001_1.mp4'
 
     return (
       <div className='section'>
-        <VideoSlide src={stadionVideo} titleBold='AEROPAN' titleRegular='FILMS' />
+        <VideoSlide src={stadionVideo} />
       </div>
     )
   }
